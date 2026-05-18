fe7599984e11dc47fd05d861e08cc34ec3626bcb
lowkeytoxic <32779104+Glebyshek@users.noreply.github.com>
2026-05-18 11:10:03 +0300

    Update SnezhnyeYagody.java

--- SnezhnyeYagody.java
+++ SnezhnyeYagody.java
@@ -1,16 +1,15 @@
 public class SnezhnyeYagody extends Dekorator {
-
     public SnezhnyeYagody(Blyudo blyudo) {
         super(blyudo);
     }
 
     @Override
     public String getNazvaniye() {
-        return blyudo.getNazvaniye() + " + Снежные ягоды";
+        return getBlyudo().getNazvaniye() + " + Снежные ягоды";
     }
 
     @Override
     public int getCena() {
-        return blyudo.getCena() + 5;
+        return getBlyudo().getCena() + 5;
     }
 }
