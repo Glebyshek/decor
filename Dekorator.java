f92734c5e4c8499a19fa332144fc8ad8bd228809
lowkeytoxic <32779104+Glebyshek@users.noreply.github.com>
2026-05-18 11:07:28 +0300

    Update Dekorator.java

--- Dekorator.java
+++ Dekorator.java
@@ -1,16 +1,10 @@
 public class Dekorator implements Blyudo {
-    protected Blyudo blyudo;
+    private Blyudo blyudo;
 
     public Dekorator(Blyudo blyudo) {
         this.blyudo = blyudo;
     }
-    @Override
-    public String getNazvaniye() {
-        return blyudo.getNazvaniye();
-    }
-
-    @Override
-    public int getCena() {
-        return blyudo.getCena();
+    public Blyudo getBlyudo() {
+        return blyudo;
     }
 }
